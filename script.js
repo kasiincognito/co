7daf00da118d2c7e692bc848ee977fbdcbe39485
kasiincognito <140201231+kasiincognito@users.noreply.github.com>
2024-09-09 14:24:23 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 var welcome = document.getElementById("welcome")
-content1 = ">>System initialisation...."
-content2 = ">>coding.exe version: 1.1 Welcome from Kasi's Dev community, if you are also a Junior developer this is a space fo ya. Commands will be availaible in next version."
-content3 = ">>Initialising shutdown sequence: bye bye!"
 var text1 = document.getElementById("text1")
 var text2 = document.getElementById("text2")
-var text3 = document.getElementById("text3")
+content1 = ">>System initialising......"
+content2 = ">>coding.exe version : 1.2 - Hello from Kasi's dev community, if you are fascinated by the world of programming, this space is for you!"
+content3 = ">>We are waiting for your support to enable commands: Bye Bye!!"
 var interval  = 10
+var counter = "_"
+var audio = document.getElementById("audio")
 
 function animate(insert, out){ //une fonction que j'ai crée pour avoir un effet de ligne de commande sur les textes
     var outList = out.textContent.split("")
@@ -18,12 +19,13 @@ function animate(insert, out){ //une fonction que j'ai crée pour avoir un effet
     }
 }
 
-animate(content1, welcome) //Je ne sais pas optimiser cette partie, aidez Kasi
+audio.play()
 setTimeout(function(){
-    animate(content2, text1)
+    animate(content1, welcome) //Je ne sais pas optimiser cette partie, aidez Kasi
     setTimeout(function(){
-        animate(content3, text2)
-    }, content2.split("").length * 30 + interval)
-}, content1.split("").length * 30 + interval)
-
-
+        animate(content2, text1)
+        setTimeout(function(){
+            animate(content3, text2)
+        }, 9400)
+    }, 5000)
+}, 3000)
